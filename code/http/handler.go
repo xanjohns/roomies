cae7723212a799ac97bda5a669aacde919d220d2
Jared Robinson <jared@smarty.com>
2024-02-22 16:48:30 -0700

    Add basic routes for the shared list

--- code/http/handler.go
+++ code/http/handler.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
@@ -10,16 +9,20 @@ type Handler struct {
 
 func (h Handler) ServeHTTP(response http.ResponseWriter, request *http.Request) {
 	switch request.RequestURI {
-	case "/pay-rent":
-		fmt.Fprintf(response, "RENT PAID")
-	case "/announcement/send":
-		fmt.Fprintf(response, "RANDOM ANNOUNCEMENT")
+	case "/grocery-list/shared/:groupID":
+		if request.Method == "GET" {
+			// GET request
+		} else {
+			// POST request
+		}
+	case "/grocer-list/shared/:groupID/:itemID":
+		// delete an item
 	}
 }
 
 func SetupRouter() http.Handler {
 	router := http.NewServeMux()
-	router.Handle("/pay-rent", Handler{})
-	router.Handle("/announcement/send", Handler{})
+	router.Handle("/grocery-list/shared/:groupID", Handler{})
+	router.Handle("/grocer-list/shared/:groupID/:itemID", Handler{})
 	return router
 }
